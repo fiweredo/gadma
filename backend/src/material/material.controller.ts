7dc1db36eab7ee7bd766d55057b7e9b6ed72eb42
Francisco Figueredo <87507650+fiweredo@users.noreply.github.com>
2025-03-04 03:06:25 +0000

    Corrección de directorio

--- backend/src/material/material.controller.ts
+++ backend/src/material/material.controller.ts
@@ -1,21 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
-import { MaterialService } from './material.service';
-import { CreateMaterialDto } from './material.dto'; // Asegúrate de importar correctamente el DTO
-import { Material } from './material.schema'; // El esquema de Material
+import { Controller } from '@nestjs/common';
 
 @Controller('material')
-export class MaterialController {
-  constructor(private readonly materialService: MaterialService) {}
-
-  // Ruta para obtener todos los materiales
-  @Get()
-  async findAll(): Promise<Material[]> {
-    return this.materialService.findAll(); // Llama al servicio para obtener todos los materiales
-  }
-
-  // Ruta para crear un nuevo material
-  @Post()
-  async create(@Body() createMaterialDto: CreateMaterialDto): Promise<Material> {
-    return this.materialService.create(createMaterialDto); // Crea un material usando el DTO
-  }
-}
+export class MaterialController {}
--- backend/src/material/material.service.ts
+++ backend/src/material/material.service.ts
@@ -1,23 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Material } from './material.schema';
-import { CreateMaterialDto } from './material.dto';
 
 @Injectable()
-export class MaterialService {
-  constructor(
-    @InjectModel(Material.name) private materialModel: Model<Material>,
-  ) {}
-
-  // Crear un material
-  async create(createMaterialDto: CreateMaterialDto): Promise<Material> {
-    const createdMaterial = new this.materialModel(createMaterialDto);
-    return createdMaterial.save(); // Guarda el material en la base de datos
-  }
-
-  // Obtener todos los materiales
-  async findAll(): Promise<Material[]> {
-    return this.materialModel.find().exec();
-  }
-}
+export class MaterialService {}
